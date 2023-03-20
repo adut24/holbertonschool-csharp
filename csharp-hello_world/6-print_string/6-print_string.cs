f1d63b1c596067e1f6be723299f3513c769c2cc5
adut24 <dutertre.alexandre@laposte.net>
2023-03-20 14:49:18 +0100

    Print a string several times and part of it

---
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+        System.Console.WriteLine("{0}", str+str+str);
+        System.Console.WriteLine("{0}", str.Substring(0, 9));
+    }
+}
