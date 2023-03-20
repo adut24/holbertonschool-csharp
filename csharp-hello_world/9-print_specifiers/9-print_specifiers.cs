547228b818c11428adc942ade375377d0a31ee50
adut24 <dutertre.alexandre@laposte.net>
2023-03-20 15:17:24 +0100

    Print the given variables

---
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		double percent = .7553;
+		double currency = 98765.4321;
+        System.Console.WriteLine("Percent: {0:P2}", percent);
+        System.Console.WriteLine("Currency: {0:C2}", currency);
+	}
+}
