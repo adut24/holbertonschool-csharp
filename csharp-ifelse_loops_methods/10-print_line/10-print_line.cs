73f677611250ba4ed2ac1622a27e91f83b1e3f18
adut24 <dutertre.alexandre@laposte.net>
2023-03-21 14:41:54 +0100

    Draw a straight line

---
+++ csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        string line = "";
+
+        for (int i = 0; i < length; i++)
+            line += "_";
+        Console.WriteLine(line);
+    }
+}
