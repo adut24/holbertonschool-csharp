a9e78ab8e46f5f241267a75a971591ff4533b0c6
adut24 <dutertre.alexandre@laposte.net>
2023-03-21 14:27:09 +0100

    Print last digit and return it

---
+++ csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Number
+{
+    public static int PrintLastDigit(int number)
+    {
+        int lastDigit = number % 10;
+        if (lastDigit < 0)
+            lastDigit = -lastDigit;
+        Console.Write(lastDigit);
+        return lastDigit;
+    }
+}
