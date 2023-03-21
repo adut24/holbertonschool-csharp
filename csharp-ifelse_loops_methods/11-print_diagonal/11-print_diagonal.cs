fd820c818ae19854b07a31a5772bf4a0adbb93af
adut24 <dutertre.alexandre@laposte.net>
2023-03-21 14:52:51 +0100

    Print a diagonal line

---
+++ csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        string line = "";
+        for (int i = 0; i < length; i++)
+        {
+            for (int j = 0; j < i; j++)
+                line += " ";
+            line += "\\\n";
+        }
+        Console.WriteLine(line);
+    }
+}
