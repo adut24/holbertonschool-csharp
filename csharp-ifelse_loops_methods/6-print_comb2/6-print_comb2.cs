c4707278f07555d263e48b78c6927fecd9fc8330
adut24 <dutertre.alexandre@laposte.net>
2023-03-21 13:45:09 +0100

    Print all different combinations of 2 digits

---
+++ csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 1; j < 10; j++)
+                {
+                    if (i < j)
+                    {
+                        Console.Write($"{i}{j}");
+                        if (i == 8 && j == 9)
+                            Console.WriteLine();
+                        else
+                            Console.Write(", ");
+                    }
+                }
+            }
+        }
+    }
+}
