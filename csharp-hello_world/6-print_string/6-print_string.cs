cf2ecf5811a3d31d28c764cd64e1ed080b3b1fff
adut24 <dutertre.alexandre@laposte.net>
2023-03-21 16:07:22 +0100

    Modification to use StringBuilder

--- csharp-hello_world/6-print_string/6-print_string.cs
+++ csharp-hello_world/6-print_string/6-print_string.cs
@@ -1,11 +1,12 @@
 ﻿using System;
+using System.Text;
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		string str = "Holberton School";
-        System.Console.WriteLine("{0}", str+str+str);
-        System.Console.WriteLine("{0}", str.Substring(0, 9));
+    static void Main(string[] args)
+    {
+        string str = "Holberton School";
+        Console.WriteLine($"{new StringBuilder(str.Length * 3).Insert(0, str, 3)}");
+        Console.WriteLine($"{str.Substring(0, 9)}");
     }
 }
