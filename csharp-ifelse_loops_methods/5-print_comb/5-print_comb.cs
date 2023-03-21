9b41e371a426a6042d7fbc618e30f2bff73f1805
adut24 <dutertre.alexandre@laposte.net>
2023-03-21 11:58:45 +0100

    Print numbers from 0 to 99

---
+++ csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                if (i < 99)
+                    System.Console.Write($"{i:D2}, ");
+                else
+                    System.Console.WriteLine($"{i}");
+            }
+        }
+    }
+}
