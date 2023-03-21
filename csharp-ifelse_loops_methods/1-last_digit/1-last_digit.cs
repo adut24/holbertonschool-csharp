a632fcb34538865f7e1fb8cdd5cc9a189ebfe004
adut24 <dutertre.alexandre@laposte.net>
2023-03-21 11:06:56 +0100

    Tell if the last digit is positive, negative or 0

---
+++ csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int lastDigit = number % 10;
+        System.Console.Write($"The last digit of {number} is {lastDigit}");
+        if (lastDigit > 5)
+            System.Console.WriteLine(" and is greater than 5");
+        else if (lastDigit == 0)
+            System.Console.WriteLine(" and is 0");
+        else
+            System.Console.WriteLine(" and is less than 6 and not 0");
+    }
+}
