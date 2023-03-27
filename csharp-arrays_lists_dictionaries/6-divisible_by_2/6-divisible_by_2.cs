95f51416ce50d1077d70f5dea6eb2ff898d3fa95
adut24 <dutertre.alexandre@laposte.net>
2023-03-27 15:25:47 +0200

    Tell if a number is divisible by 2 or not

---
+++ csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> isDivisibleBy2 = new List<bool>();
+        isDivisibleBy2.AddRange(myList.ConvertAll(n => n % 2 == 0));
+        return isDivisibleBy2;
+    }
+}
