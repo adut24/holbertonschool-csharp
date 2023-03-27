cddc717b9bba7bfbfc6157117a7d032dd193f126
adut24 <dutertre.alexandre@laposte.net>
2023-03-27 10:15:45 +0200

    Create and print an array

---
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return null;
+        }
+        if (size == 0)
+            Console.WriteLine();
+
+        int[] numbers = Enumerable.Range(0, size).ToArray();
+        int lengthArray = numbers.Length;
+        for (int i = 0; i < lengthArray; i++)
+        {
+            if (i < lengthArray - 1)
+                Console.Write($"{numbers[i]} ");
+            else
+                Console.WriteLine($"{numbers[i]}");
+        }
+        return numbers;
+    }
+}
