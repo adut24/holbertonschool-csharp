924f621536cf78a42f0afe5f9b3354d912c7d9f6
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 13:50:05 +0200

    Return a list of elements present in 2 lists

---
+++ csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> commonElements = new List<int>();
+
+        foreach (int number in list1)
+        {
+            if (list2.Contains(number) && !commonElements.Contains(number))
+                commonElements.Add(number);
+        }
+        return commonElements;
+    }
+}
