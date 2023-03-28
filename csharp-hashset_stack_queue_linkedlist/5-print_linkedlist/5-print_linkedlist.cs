f44b074f70b78f969c3bb2a3e71698fbb3d8f13b
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 17:26:29 +0200

    Create and print a linked list

---
+++ csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> numbers = new LinkedList<int>();
+        if (size < 0)
+            return numbers;
+        for (int i = 0; i < size; i++)
+        {
+            numbers.AddLast(i);
+            System.Console.WriteLine(numbers.ElementAt(i));
+        }
+        return numbers;
+    }
+}
