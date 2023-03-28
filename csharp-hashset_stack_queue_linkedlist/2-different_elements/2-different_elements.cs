03a551858eedb9f066ff245dd6cbfe2009bf11b6
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 14:52:52 +0200

    Return a list of elements present in 1 list

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> differentElements = new List<int>();
+        foreach (int number in list1)
+        {
+            if (!list2.Contains(number) && !differentElements.Contains(number))
+                differentElements.Add(number);
+        }
+        foreach (int number in list2)
+        {
+            if (!list1.Contains(number) && !differentElements.Contains(number))
+                differentElements.Add(number);
+        }
+        differentElements.Sort();
+        return differentElements;
+    }
+}
