f0b4a8de15feb399524147bbc2aea9d7ee315d40
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 10:44:30 +0200

    Check the student with the best score

---
+++ csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+	public static string BestScore(Dictionary<string, int> myDict)
+	{
+		int bestScore = -1;
+		string key = "None";
+
+		foreach (KeyValuePair<string, int> kvp in myDict)
+		{
+			int value = kvp.Value;
+			if (value > bestScore)
+			{
+				bestScore = value;
+				key = kvp.Key;
+			}
+		}
+	   return key;
+	}
+}
+
