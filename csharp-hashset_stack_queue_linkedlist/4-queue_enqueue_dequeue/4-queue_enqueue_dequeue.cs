c1fb18a561f3466c6c49129c30ff68bdcf17e704
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 16:15:30 +0200

    Print informations about a queue

---
+++ csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        int count = aQueue.Count;
+        Console.WriteLine($"Number of items: {count}");
+        if (count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine($"Top item: {aQueue.Peek()}");
+        bool hasObject = aQueue.Contains(search);
+        Console.WriteLine($"Queue contains \"{search}\": {hasObject}");
+        if (hasObject)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                if (aQueue.Peek() == search)
+                    i = count;
+                aQueue.Dequeue();
+            }
+        }
+        aQueue.Enqueue(newItem);
+        return aQueue;
+    }
+}
