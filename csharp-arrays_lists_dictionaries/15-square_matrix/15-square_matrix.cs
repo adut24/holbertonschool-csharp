c75e8c5554af09ebc3be444c2472b7c0d60b79e0
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 11:30:09 +0200

    Square each numbers present in the matrix

---
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int[] squaredMatrix = myMatrix.Cast<int>().Select(n => n * n).ToArray();
+        int[,] newMatrix = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+        Buffer.BlockCopy(squaredMatrix, 0, newMatrix, 0, squaredMatrix.Length * sizeof(int));
+        return newMatrix;
+    }
+}
