fbc5720fb66ac06a89c04e0442d504e6c8362cba
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 10:09:03 +0200

    Print a dictionary sorted by key

---
+++ csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        List<string> keysSorted = myDict.Keys.ToList();
+        keysSorted.Sort();
+        foreach (string key in keysSorted)
+        {
+            Console.WriteLine($"{key}: {myDict[key]}");
+        }
+    }
+}
