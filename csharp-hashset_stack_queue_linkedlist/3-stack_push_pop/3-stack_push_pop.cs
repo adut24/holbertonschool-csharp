84037c77d1360050a86164b2919a8522c85317b6
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 15:31:46 +0200

    Print informations about the stack

---
+++ csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int count = aStack.Count;
+        Console.WriteLine($"Number of items: {count}");
+        if (count == 0)
+            System.Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+        bool hasObject = aStack.Contains(search);
+        Console.WriteLine($"Stack contains \"{search}\": {hasObject}");
+        for (int i = 0; i < count; i++)
+        {
+            if (aStack.Peek() == search)
+                i = count;
+            aStack.Pop();
+        }
+        aStack.Push(newItem);
+        return aStack;
+    }
+}
