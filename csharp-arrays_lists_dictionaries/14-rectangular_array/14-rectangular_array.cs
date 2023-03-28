7c834a93a05011f657d6b0c58fbc36b91e272ee1
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 11:02:20 +0200

    Create and print a 2D array

---
+++ csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _14_rectangular_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] numbers = new int[5, 5];
+            numbers[2, 2] = 1;
+            for (int i = 0; i < numbers.GetLength(0); i++)
+            {
+                for (int j = 0; j < numbers.GetLength(1); j++)
+                    Console.Write(numbers[i, j] + " ");
+                Console.WriteLine();
+            }
+        }
+    }
+}
