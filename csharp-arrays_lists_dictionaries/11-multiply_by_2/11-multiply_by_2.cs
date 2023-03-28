663ef55e71a5ff32fdadc6d607a03e6615a22675
adut24 <dutertre.alexandre@laposte.net>
2023-03-28 10:02:09 +0200

    Multiply all values of a dictionary by 2

---
+++ csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Dictionary
+{
+    public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
+    {
+        return myDict.ToDictionary(keyValue => keyValue.Key, keyValue => keyValue.Value *2);
+    }
+}
