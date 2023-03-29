fa3d4ac69cf5a204b723425c6fb47f215c76af61
adut24 <dutertre.alexandre@laposte.net>
2023-03-29 10:33:01 +0200

    Return the value of the n node

---
+++ csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+	public static int GetNode(LinkedList<int> myLList, int n)
+	{
+        if (myLList.Count <= n)
+			return 0;
+		return myLList.ElementAt(n);
+	}
+}
