c9f22f229b810d14fe2af334160f853f0d744b12
adut24 <dutertre.alexandre@laposte.net>
2023-03-29 11:13:05 +0200

    Remove a node at a given index

---
+++ csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+	public static void Delete(LinkedList<int> myLList, int index)
+	{
+        if (index < 0 || index >= myLList.Count)
+			return;
+		myLList.Remove(myLList.ElementAt(index));
+	}
+}
