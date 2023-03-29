b2c7637b653389afd4d58c3e8f13597d923d44c1
adut24 <dutertre.alexandre@laposte.net>
2023-03-29 10:03:41 +0200

    Find at which index is the value in the LinkedList

---
+++ csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+class LList
+{
+	public static int FindNode(LinkedList<int> myLList, int value)
+	{
+		int index = myLList.TakeWhile(node => value != node).Count();
+
+		if (index == myLList.Count)
+			return -1;
+		return index;
+	}
+}
