428945f4be533d5ecb1937ad9ac1e85a289ba8b9
adut24 <dutertre.alexandre@laposte.net>
2023-03-29 10:59:36 +0200

    Insert a node in a sorted LinkedList

---
+++ csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+class LList
+{
+	public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+	{
+		LinkedListNode<int> newNode = new LinkedListNode<int>(n);
+		foreach (int value in myLList)
+		{
+			if (value > newNode.Value)
+			{
+				myLList.AddBefore(myLList.Find(value), newNode);
+				break;
+			}
+		}
+		return newNode;
+	}
+}
