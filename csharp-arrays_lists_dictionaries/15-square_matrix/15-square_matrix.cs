78f91a7d8102266a1bc0ccbc52369fd5918d20e0
adut24 <dutertre.alexandre@laposte.net>
2023-03-30 14:02:52 +0200

    Adding comments

--- csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
+++ csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -5,8 +5,11 @@ class Matrix
 {
     public static int[,] Square(int[,] myMatrix)
     {
+        /* Create an array containing the square value of all integers inside myMatrix */
         int[] squaredMatrix = myMatrix.Cast<int>().Select(n => n * n).ToArray();
+        /* Create a new matrix of the same size as the original one */
         int[,] newMatrix = new int[myMatrix.GetLength(0), myMatrix.GetLength(1)];
+        /* Copy all bytes from squaredMatrix to newMatrix */
         Buffer.BlockCopy(squaredMatrix, 0, newMatrix, 0, squaredMatrix.Length * sizeof(int));
         return newMatrix;
     }
