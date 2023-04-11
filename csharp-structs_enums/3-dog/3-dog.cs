c2887ad2fce3492ff24fff1500ed4272863825f7
adut24 <dutertre.alexandre@laposte.net>
2023-04-11 11:00:38 +0200

    Override the ToString() method

---
+++ csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,32 @@
+﻿enum Rating
+{
+	Good,
+	Great,
+	Excellent
+}
+
+struct Dog
+{
+	public string name;
+	public float age;
+	public string owner;
+	public Rating rating;
+	public Dog(string name, float age, string owner, Rating rating)
+	{
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+	}
+
+	public override string ToString()
+	{
+		string infoPrinted = $"Dog Name: {this.name}\n";
+
+        infoPrinted += $"Age: {this.age}\n";
+        infoPrinted += $"Owner: {this.owner}\n";
+        infoPrinted += $"Rating: {this.rating}";
+
+        return infoPrinted;
+	}
+}
