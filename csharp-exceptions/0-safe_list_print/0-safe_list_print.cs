dd173ac0c7554e5415e98316ae99aed2ddd86b9f
adut24 <dutertre.alexandre@laposte.net>
2023-04-11 09:54:46 +0200

    Print elements of a List safely

---
+++ csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+	public static int SafePrint(List<int> myList, int n)
+	{
+		int elementsPrinted = 0;
+
+		for (int i = 0; i < n; i++)
+		{
+			try
+			{
+				Console.WriteLine(myList[i]);
+				elementsPrinted++;
+			}
+			catch (IndexOutOfRangeException)
+			{
+			}
+		}
+		return elementsPrinted;
+	}
+}
