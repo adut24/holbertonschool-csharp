f59eb28d5f9698299eff58702dd643f3424977d8
adut24 <dutertre.alexandre@laposte.net>
2023-04-18 17:52:59 +0200

    Divide a matrix

---
+++ csharp-tdd/1-divide/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,64 @@
+using MyMath;
+using NUnit.Framework;
+
+namespace Tests
+{
+	public class Tests
+	{
+
+		[Test]
+		public void TestWithNormalDivide()
+		{
+			int[,] matrix = new int[,]
+			{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+			};
+			int[,] result = Matrix.Divide(matrix, 2);
+			int[,] expectedResult = new int[,]
+			{
+				{0, 1, 1},
+				{2, 2, 3},
+                {3, 4, 4}
+			};
+			Assert.AreEqual(expectedResult, result);
+		}
+
+        [Test]
+        public void TestWithDivideByZero()
+		{
+			int[,] matrix = new int[,]
+			{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+			};
+			int[,] result = Matrix.Divide(matrix, 0);
+			Assert.AreEqual(null, result);
+		}
+
+        [Test]
+        public void TestWithMatrixNull()
+        {
+			int[,] result = Matrix.Divide(null, 9);
+			Assert.AreEqual(null, result);
+		}
+		{
+			int[,] matrix = new int[,]
+			{
+				{1, 2, 3},
+				{4, 5, 6},
+				{7, 8, 9}
+			};
+			int[,] result = Matrix.Divide(matrix, -2);
+			int[,] expectedResult = new int[,]
+			{
+				{0, -1, -1},
+				{-2, -2, -3},
+				{-3, -4, -4}
+			};
+			Assert.AreEqual(expectedResult, result);
+		}
+	}
+}
---
+++ csharp-tdd/1-divide/MyMath/MyMath.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyMath
+{
+	/// <summary>
+	/// Represents the different actions possible with a matrix.
+	/// </summary>
+	public static class Matrix
+	{
+		/// <summary>
+		/// Divides all elements in <paramref name="matrix"/> by <paramref name="num"/>.
+		/// </summary>
+		/// <param name="matrix">Matrix to divide.</param>
+		/// <param name="num">Integer to divide matrix by.</param>
+		/// <returns> The new matrix or null</returns>
+		/// <exception cref="DivideByZeroException"><paramref name="num"/> can't be 0.</exception>
+		public static int[,] Divide(int[,] matrix, int num)
+		{
+			int[] matrixDivided;
+			int[,] newMatrix;
+
+			if (matrix == null)
+				return null;
+
+			try
+			{
+				matrixDivided = matrix.Cast<int>().AsEnumerable().Select(number => number / num).ToArray();
+				newMatrix = new int[matrix.GetLength(0), matrix.GetLength(1)];
+				Buffer.BlockCopy(matrixDivided, 0, newMatrix, 0, matrixDivided.Length * sizeof(int));
+			}
+			catch (DivideByZeroException)
+			{
+				Console.WriteLine("Num cannot be 0");
+				return null;
+			}
+			return newMatrix;
+		}
+	}
+}
