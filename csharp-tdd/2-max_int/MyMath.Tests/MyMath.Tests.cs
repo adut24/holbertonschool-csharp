b6fb212135955c252ff9cc53450690d3eed506e3
adut24 <dutertre.alexandre@laposte.net>
2023-04-18 19:31:23 +0200

    Return the max value in a list

---
+++ csharp-tdd/2-max_int/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using MyMath;
+using NUnit.Framework;
+
+namespace MyMath.Tests
+{
+    public class Tests
+    {
+        /// <summary>
+        /// Tests with only positive int.
+        /// </summary>
+        [Test]
+        public void TestWithOnlyPositiveInt()
+        {
+			List<int> numbers = new List<int>() { 0, 5, 4, 43, 3, 5};
+			int result = Operations.Max(numbers);
+			Assert.AreEqual(43, result);
+		}
+
+		/// <summary>
+		/// Tests with an empty list.
+		/// </summary>
+		[Test]
+		public void TestWithEmptyList()
+		{
+			List<int> numbers = new List<int>();
+			int result = Operations.Max(numbers);
+			Assert.AreEqual(0, result);
+		}
+
+		/// <summary>
+		/// Tests with only the same value.
+		/// </summary>
+		[Test]
+		public void TestWithOnlyMaxValue()
+		{
+			List<int> numbers = new List<int>() { 5, 5, 5};
+			int result = Operations.Max(numbers);
+			Assert.AreEqual(5, result);
+		}
+
+		/// <summary>
+		/// Tests with only negative int.
+		/// </summary>
+		[Test]
+		public void TestWithOnlyNegativeInt()
+		{
+			List<int> numbers = new List<int>() { -5, -4, -43, -3, -5 };
+			int result = Operations.Max(numbers);
+			Assert.AreEqual(-3, result);
+		}
+
+		/// <summary>
+		/// Tests with only one int.
+		/// </summary>
+		[Test]
+		public void TestWithOneValue()
+		{
+			List<int> numbers = new List<int>() { -5 };
+			int result = Operations.Max(numbers);
+			Assert.AreEqual(-5, result);
+		}
+    }
+}
---
+++ csharp-tdd/2-max_int/MyMath/MyMath.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyMath
+{
+    /// <summary>
+    /// Represents possible operations on numbers.
+    /// </summary>
+    public static class Operations
+    {
+        /// <summary>
+        /// Finds the max value in a list of int.
+        /// </summary>
+        /// <param name="nums">List of int.</param>
+        /// <returns>The max value</returns>
+		public static int Max(List<int> nums)
+        {
+            if (nums.Count == 0)
+				return 0;
+			return nums.Max();
+		}
+    }
+}
