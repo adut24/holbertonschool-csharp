f648f11f888cf44d5229728d98d1b29e30320486
adut24 <dutertre.alexandre@laposte.net>
2023-04-19 09:58:52 +0200

    Counts the number of words in a string

---
+++ csharp-tdd/5-camelcase/Text.Tests/Text.Tests.cs
@@ -0,0 +1,29 @@
+using NUnit.Framework;
+using Text;
+
+namespace Text.Tests
+{
+	public class Tests
+	{
+		[Test]
+		public void TestWithOneWord()
+		{
+			int result = Str.CamelCase("hello");
+			Assert.AreEqual(1, result);
+		}
+
+		[Test]
+		public void TestWithSeveralWords()
+		{
+			int result = Str.CamelCase("holbertonSchoolCsharpProject");
+			Assert.AreEqual(4, result);
+		}
+
+		[Test]
+		public void TestWithEmptyString()
+		{
+			int result = Str.CamelCase("");
+			Assert.AreEqual(0, result);
+		}
+	}
+}
---
+++ csharp-tdd/5-camelcase/Text/Text.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace Text
+{
+	/// <summary>
+	/// Represents a string checker.
+	/// </summary>
+	public static class Str
+	{
+		/// <summary>
+		/// Checks how many words are in the string.
+		/// </summary>
+		/// <param name="s">String to check.</param>
+		/// <returns>The number of words in the string.</returns>
+		public static int CamelCase(string s) => String.IsNullOrEmpty(s) ? 0 : s.AsEnumerable().Where(c => Char.IsUpper(c)).Count() + 1;
+	}
+}
