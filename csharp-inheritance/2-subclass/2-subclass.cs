01e533619440ca7eb52fba43e8dc078a78133b31
adut24 <dutertre.alexandre@laposte.net>
2023-04-24 14:11:31 +0200

    Correction

--- csharp-inheritance/2-subclass/2-subclass.cs
+++ csharp-inheritance/2-subclass/2-subclass.cs
@@ -11,5 +11,5 @@ static class Obj
 	/// <param name="derivedType">Class to check</param>
 	/// <param name="baseType">Reference class</param>
 	/// <returns>True if <paramref name="derivedType"/> inherits from <paramref name="baseType"/></returns>
-	public static bool IsOnlyASubclass(Type derivedType, Type baseType) => derivedType.BaseType == baseType;
+	public static bool IsOnlyASubclass(Type derivedType, Type baseType) => derivedType.IsSubclassOf(baseType);
 }
