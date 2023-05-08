0ecb4ebc20db3574e087d757683efe4f635e458e
adut24 <dutertre.alexandre@laposte.net>
2023-05-08 20:17:42 +0200

    Calculates the magnitude of a vector

---
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+/// <summary>
+/// Represents operations on vectors.
+/// </summary>
+class VectorMath
+{
+    /// <summary>
+    /// Calculates the length of a 2D or 3D vector.
+    /// </summary>
+    /// <param name="vector">Vector to calculate</param>
+    /// <returns>-1 or the value of the magnitude.</returns>
+	public static double Magnitude(double[] vector)
+	{
+		double vectorLength = 0;
+
+		if (vector.Length != 2 && vector.Length != 3)
+			return -1;
+        foreach (double point in vector)
+		{
+			vectorLength += point * point;
+		}
+		vectorLength = Math.Sqrt(vectorLength);
+		return Math.Round(vectorLength, 2);
+	}
+}
