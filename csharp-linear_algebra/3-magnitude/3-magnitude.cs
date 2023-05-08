171c0c5fb9a0297411cdcd83bad90720676552ce
adut24 <dutertre.alexandre@laposte.net>
2023-05-08 20:55:07 +0200

    Correction of indentation

--- csharp-linear_algebra/3-magnitude/3-magnitude.cs
+++ csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -5,18 +5,18 @@
 /// </summary>
 class VectorMath
 {
-    /// <summary>
-    /// Calculates the length of a 2D or 3D vector.
-    /// </summary>
-    /// <param name="vector">Vector to calculate</param>
-    /// <returns>-1 or the value of the magnitude.</returns>
+	/// <summary>
+	/// Calculates the length of a 2D or 3D vector.
+	/// </summary>
+	/// <param name="vector">Vector to calculate</param>
+	/// <returns>-1 or the value of the magnitude.</returns>
 	public static double Magnitude(double[] vector)
 	{
 		double vectorLength = 0;
 
 		if (vector.Length != 2 && vector.Length != 3)
 			return -1;
-        foreach (double point in vector)
+		foreach (double point in vector)
 		{
 			vectorLength += point * point;
 		}
