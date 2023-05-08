1497b93139b6a4d1ec7b9189456836c0b5be2147
adut24 <dutertre.alexandre@laposte.net>
2023-05-08 20:58:47 +0200

    Correction of doc and array initialization

--- csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
+++ csharp-linear_algebra/6-vector_addition/6-vector_addition.cs
@@ -8,15 +8,15 @@ class VectorMath
 	/// <summary>
 	/// Adds two vector together.
 	/// </summary>
-	/// <param name="vector1"></param>
-	/// <param name="vector2"></param>
-	/// <returns></returns>
+	/// <param name="vector1">First vector to add</param>
+	/// <param name="vector2">Second vector to add</param>
+	/// <returns>-1 or the sum of the 2 vectors.</returns>
 	public static double[] Add(double[] vector1, double[] vector2)
 	{
 		if (vector1.Length != 2 && vector1.Length != 3 && vector2.Length != 2 && vector2.Length && vector1.Length != vector2.Length)
 			return -1;
 
-		double[] sum = new double[];
+		double[] sum = new double[vector1.Length];
 
 		for (int i = 0; i < vector1.Length; i++)
 		{
