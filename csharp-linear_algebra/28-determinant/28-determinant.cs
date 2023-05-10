026a4a525237d8936ce19b5185a924b85a9bd584
adut24 <dutertre.alexandre@laposte.net>
2023-05-10 13:35:59 +0200

    Calculates the determinant of a matrix

---
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+/// <summary>
+/// Represents operations on matrixes.
+/// </summary>
+class MatrixMath
+{
+    /// <summary>
+    /// Calculates the determinant of a 2D or 3D matrix.
+    /// </summary>
+    /// <param name="matrix">2D or 3D matrix</param>
+    /// <returns>-1 or the determinant.</returns>
+	public static double Determinant(double[,] matrix)
+	{
+		int matrixChecked = CheckMatrix(matrix);
+
+        if (matrixChecked != 2 && matrixChecked != 3)
+			return -1;
+
+        if (matrixChecked == 2)
+			return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+
+		double n1 = matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1]);
+		double n2 = matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0]);
+		double n3 = matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
+		return n1 - n2 + n3;
+	}
+
+	/* Check if a matrix is 2D, 3D or anything else */
+	private static int CheckMatrix(double[,] matrix)
+	{
+		int length = matrix.GetLength(0);
+
+		if (length != 2 && length != 3)
+			return -1;
+
+		if (matrix.GetLength(1) != length)
+			return -1;
+
+		return length;
+	}
+}
