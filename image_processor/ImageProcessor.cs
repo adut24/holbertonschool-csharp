a0988fbaa0e781f7726c3ecfde7c14cd4cf85f13
adut24 <dutertre.alexandre@laposte.net>
2023-05-18 15:10:29 +0200

    Test

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,7 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Drawing;
-using System.Drawing.Imaging;
-using System.Runtime.InteropServices;
 using System.Threading;
 
 /// <summary> Represents the possible actions on images. </summary>
@@ -36,27 +34,26 @@ class ImageProcessor
         string extension = nameSplit[1];
 
         Bitmap image = new Bitmap(filename);
-        BitmapData imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, image.PixelFormat);
-
-        int imageSize = imageData.Stride * image.Height;
-        byte[] imageBytes = new byte[imageSize];
-
-        /* Copies all the bytes from the image to the new array to modify them */
-        Marshal.Copy(imageData.Scan0, imageBytes, 0, imageSize);
 
         /* Inverts the pixels */
-        for (int i = 0; i < imageSize; i += 4)
+        for (int y = 0; y < image.Height; y++)
         {
-            imageBytes[i] = (byte)(255 - imageBytes[i]);
-            imageBytes[i + 1] = (byte)(255 - imageBytes[i + 1]);
-            imageBytes[i + 2] = (byte)(255 - imageBytes[i + 2]);
+            for (int x = 0; x < image.Width; x++)
+            {
+                Color originalColor = image.GetPixel(x, y);
+                Color invertedColor = Color.FromArgb(
+                    255 - originalColor.R,
+                    255 - originalColor.G,
+                    255 - originalColor.B
+                );
+                image.SetPixel(x, y, invertedColor);
+            }
         }
-        /* Returns all the new bytes to the bitmap to create the new image */
-        Marshal.Copy(imageBytes, 0, imageData.Scan0, imageSize);
-        image.UnlockBits(imageData);
+
         string invertedImagePath = fileName + "_inverse." + extension;
+        
         image.Save(invertedImagePath);
-        /* Free the  */
+        /* Free the image */
         image.Dispose();
     }
 }
