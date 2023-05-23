0578493b1761ef048c9a5a3aa617fe1b4e03ce3e
adut24 <dutertre.alexandre@laposte.net>
2023-05-23 16:31:41 +0200

    Optimization

--- image_processor/ImageProcessor.cs
+++ image_processor/ImageProcessor.cs
@@ -1,58 +1,70 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 
 /// <summary> Represents the possible actions on images. </summary>
-class ImageProcessor
+static class ImageProcessor
 {
     /// <summary> Inverts the colors on the images contained in the array <paramref name="filenames"/>.</summary>
     /// <param name="filenames">Array of path to the images.</param>
-    public static void Inverse(string[] filenames) => InvertColorsInParallel(filenames);
-
-    /* Inverts the images' colors in different threads and waits for the end of each threads. */
-    private static void InvertColorsInParallel(string[] filenames)
+    public static void Inverse(string[] filenames)
     {
-        List<Task> tasks = new List<Task>();
+        int numberImages = filenames.Length;
 
-        foreach (string filename in filenames)
-            tasks.Add(Task.Run(() => InvertColors(filename)));
+        Task[] tasks = new Task[numberImages];
 
-        Task.WaitAll(tasks.ToArray());
+        for (int i = 0; i < numberImages; i++)
+        {
+            int index = i;
+            tasks[i] = Task.Run(() => InvertColors(filenames[index]));
+        }
+
+        Task.WaitAll(tasks);
     }
 
     /* Inverts the colors of the image. */
    private static void InvertColors(string filename)
     {
-        string basename = Path.GetFileName(filename);
-        string[] nameSplit = basename.Split('.');
+        const string basePath = "images/";
+        string baseName = Path.GetFileName(filename);
+        string[] nameSplit = baseName.Split('.');
         string name = nameSplit[0];
         string extension = nameSplit[1];
 
-        Bitmap image = new Bitmap("images/" + basename);
-        BitmapData imageData = image.LockBits(new Rectangle(0, 0, image.Width, image.Height), ImageLockMode.ReadWrite, image.PixelFormat);
-
-        int imageSize = imageData.Stride * image.Height;
-        byte[] imageBytes = new byte[imageSize];
-
-        /* Copies all the bytes from the image to the new array to modify them */
-        Marshal.Copy(imageData.Scan0, imageBytes, 0, imageSize);
-
-        /* Inverts the pixels */
-        for (int i = 0; i < imageSize; i += 4)
+        using (Bitmap image = new Bitmap(Path.Combine(basePath, baseName)))
         {
-            imageBytes[i] = (byte)(255 - imageBytes[i]);
-            imageBytes[i + 1] = (byte)(255 - imageBytes[i + 1]);
-            imageBytes[i + 2] = (byte)(255 - imageBytes[i + 2]);
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            BitmapData imageData = image.LockBits(rect, ImageLockMode.ReadWrite, image.PixelFormat);
+            int stride = imageData.Stride;
+            int height = image.Height;
+
+            byte[] imageBytes = new byte[stride * height];
+            int lengthBytes = imageBytes.Length;
+
+            /* Copies all the bytes from the image to the new array to modify them */
+            Marshal.Copy(imageData.Scan0, imageBytes, 0, lengthBytes);
+
+            /* Inverts the pixels */
+            for (int y = 0; y < height; y++)
+            {
+                int rowOffset = y * stride;
+
+                for (int x = 0; x < stride; x += 4)
+                {
+                    int offset = rowOffset + x;
+                    imageBytes[offset] = (byte)(255 - imageBytes[offset]);
+                    imageBytes[offset + 1] = (byte)(255 - imageBytes[offset + 1]);
+                    imageBytes[offset + 2] = (byte)(255 - imageBytes[offset + 2]);
+                }
+            }
+
+            /* Returns all the new bytes to the bitmap to create the new image */
+            Marshal.Copy(imageBytes, 0, imageData.Scan0, lengthBytes);
+            image.UnlockBits(imageData);
+
+            image.Save($"{name}_inverse.{extension}");
         }
-        /* Returns all the new bytes to the bitmap to create the new image */
-        Marshal.Copy(imageBytes, 0, imageData.Scan0, imageSize);
-        image.UnlockBits(imageData);
-        string invertedImagePath = name + "_inverse." + extension;
-        image.Save(invertedImagePath);
-        /* Free the image */
-        image.Dispose();
     }
 }
