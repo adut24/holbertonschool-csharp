e30cb419efbad622f31c1a8834a5d51fbffc11c4
adut24 <dutertre.alexandre@laposte.net>
2023-07-03 22:58:47 +0200

    Remove the head of the queue

---
+++ csharp-generics/2-dequeue/queue.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+/// <summary>
+/// Represents a queue of type T.
+/// </summary>
+/// <typeparam name="T">Type of data stored.</typeparam>
+public class Queue<T>
+{
+    private Node head;
+    private Node tail;
+    private int count;
+
+    /// <summary>
+    /// Returns the queue's type.
+    /// </summary>
+    /// <returns>The queue's type</returns>
+    public Type CheckType() => typeof(T);
+
+    /// <summary>
+    /// Represents a node in the queue.
+    /// </summary>
+    public class Node
+    {
+        internal T value = default(T);
+        internal Node next = default(Node);
+
+        /// <summary>
+        /// Constructor for the node.
+        /// </summary>
+        public Node(T input) => value = input;
+    }
+
+    /// <summary>
+    /// Adds a node to the queue.
+    /// </summary>
+    /// <param name="value"></param>
+    public void Enqueue(T value)
+    {
+        Node node = new Node(value);
+
+        if (head == null)
+            head = node;
+        else
+        {
+            while (head.next != null)
+                head = head.next;
+            head.next = node;
+        }
+        tail = node;
+        count++;
+    }
+
+    /// <summary>
+    /// Gets the number of nodes in the queue.
+    /// </summary>
+    /// <returns>Number of nodes in the queue.</returns>
+    public int Count() => count;
+
+    /// <summary>
+    /// Deletes the first node and returns its value.
+    /// </summary>
+    /// <returns>The value of the node.</returns>
+    public T Dequeue()
+    {
+        T value;
+
+        if (head == null)
+        {
+            Console.WriteLine("Queue is empty");
+            return default(T);
+        }
+        value = head.value;
+        head = head.next;
+        if (head == null)
+            tail = null;
+        count--;
+        return value;
+    }
+}
