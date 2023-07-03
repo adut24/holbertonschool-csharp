318c3052200d280c20bbcef5cc7af88d23ec2df4
adut24 <dutertre.alexandre@laposte.net>
2023-07-03 15:36:01 +0200

    Return the type of what is contained in the queue

---
+++ csharp-generics/0-queue/queue.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+/// <summary>
+/// Represents a Queue of a certain type of data.
+/// </summary>
+/// <typeparam name="T">Type of data stored.</typeparam>
+public class Queue<T>
+{
+    /// <summary>
+    /// Returns the Queue's type.
+    /// </summary>
+    /// <returns>The Queue's type</returns>
+    public Type CheckType()
+    {
+        return typeof(T);
+    }
+}
