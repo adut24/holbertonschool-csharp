09f3e696e9a520e5321d946af519fffb657b2ea2
adut24 <dutertre.alexandre@laposte.net>
2023-07-03 22:18:57 +0200

    Adds elements to the queue

---
+++ csharp-generics/1-enqueue/queue.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+/// <summary>
+/// Represents a queue of type T.
+/// </summary>
+/// <typeparam name="T">Type of data stored.</typeparam>
+public class Queue<T>
+{
+    private Node head;
+    private Node tail;
+    private int count;
+
+    /// <summary>
+    /// Returns the queue's type.
+    /// </summary>
+    /// <returns>The queue's type</returns>
+    public Type CheckType() => typeof(T);
+
+    /// <summary>
+    /// Represents a node in the queue.
+    /// </summary>
+    public class Node
+    {
+        internal T value = default;
+        internal Node next = default;
+
+        /// <summary>
+        /// Constructor for the node.
+        /// </summary>
+        public Node(T input) => value = input;
+    }
+
+    /// <summary>
+    /// Adds a node to the queue.
+    /// </summary>
+    /// <param name="value"></param>
+    public void Enqueue(T value)
+    {
+        Node node = new Node(value);
+
+        if (head == null)
+            head = node;
+        else
+        {
+            while (head.next != null)
+                head = head.next;
+            head.next = node;
+        }
+        count++;
+    }
+
+    /// <summary>
+    /// Gets the number of nodes in the queue.
+    /// </summary>
+    /// <returns>Number of nodes in the queue.</returns>
+    public int Count() => count;
+}
