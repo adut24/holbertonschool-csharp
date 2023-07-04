5f1f5521e4ff3644cb0f5506a81122069a40bdf2
adut24 <dutertre.alexandre@laposte.net>
2023-07-04 14:07:03 +0200

    Implements three interfaces into a class

---
+++ csharp-interfaces/1-user_interface/1-user_interface.cs
@@ -0,0 +1,56 @@
+﻿/// <summary>
+/// Represents the base to build the classes.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Represents the name of the instance.
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Returns a string representation of the instance.
+    /// </summary>
+    /// <returns>The string representation of the instance.</returns>
+    public override string ToString() => $"{name} is a {GetType()}";
+}
+
+/// <summary>
+///
+/// </summary>
+public interface IInteractive
+{
+    void Interact();
+}
+
+public interface IBreakable
+{
+    int durability { get; set; }
+
+    void Break();
+}
+
+public interface ICollectable
+{
+    bool isCollected { get; set; }
+
+    void Collect();
+}
+
+public class TestObject : Base, IInteractive, IBreakable, ICollectable
+{
+    public int durability { get; set; }
+    public bool isCollected { get; set; }
+
+    public void Interact()
+    {
+    }
+
+    public void Break()
+    {
+    }
+
+    public void Collect()
+    {
+    }
+}
