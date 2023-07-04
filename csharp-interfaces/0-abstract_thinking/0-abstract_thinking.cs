2c7d553454750c7afa63cafc24e08c6c77e594f6
adut24 <dutertre.alexandre@laposte.net>
2023-07-04 11:56:50 +0200

    Define an abstract class named Base

---
+++ csharp-interfaces/0-abstract_thinking/0-abstract_thinking.cs
@@ -0,0 +1,16 @@
+﻿/// <summary>
+/// Represents the base to build the classes.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Represents the name of the instance.
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Returns a string representation of the instance.
+    /// </summary>
+    /// <returns>The string representation of the instance.</returns>
+    public override string ToString() => $"{name} is a {GetType()}";
+}
