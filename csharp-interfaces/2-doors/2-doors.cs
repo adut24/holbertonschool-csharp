f05a961224210011878b2d1086b07568542264ae
adut24 <dutertre.alexandre@laposte.net>
2023-07-04 17:25:20 +0200

    Creates a class representating an interactive door

---
+++ csharp-interfaces/2-doors/2-doors.cs
@@ -0,0 +1,79 @@
+﻿using System;
+/// <summary>
+/// Represents the base to build the classes.
+/// </summary>
+public abstract class Base
+{
+    /// <summary>
+    /// Represents the name of the instance.
+    /// </summary>
+    public string name;
+
+    /// <summary>
+    /// Returns a string representation of the instance.
+    /// </summary>
+    /// <returns>The string representation of the instance.</returns>
+    public override string ToString() => $"{name} is a {GetType()}";
+}
+
+/// <summary>
+/// Represents an interactive object.
+/// </summary>
+public interface IInteractive
+{
+    /// <summary>
+    /// Interacts with the object.
+    /// </summary>
+    void Interact();
+}
+
+/// <summary>
+/// Represents a breakable object.
+/// </summary>
+public interface IBreakable
+{
+    /// <summary>
+    /// Gets and sets the durability.
+    /// </summary>
+    /// <value>Durability value</value>
+    int durability { get; set; }
+
+    /// <summary>
+    /// Breaks the breakable object
+    /// </summary>
+    void Break();
+}
+
+/// <summary>
+/// Represents a collectable object.
+/// </summary>
+public interface ICollectable
+{
+    /// <summary>
+    /// Gets and sets if the collectable was collected.
+    /// </summary>
+    /// <value>True or False</value>
+    bool isCollected { get; set; }
+
+    /// <summary>
+    /// Collects the collectable.
+    /// </summary>
+    void Collect();
+}
+
+/// <summary>
+/// Represents an interactive door.
+/// </summary>
+public class Door : Base, IInteractive
+{
+    /// <summary>
+    /// Constructor for the Door class.
+    /// </summary>
+    /// <param name="name">Name of the door.</param>
+    public Door(string name = "Door") => this.name = name;
+
+    /// <summary>
+    /// Interacts with the object.
+    /// </summary>
+    public void Interact() => Console.WriteLine($"You try to open the {name}. It's locked.");
+}
