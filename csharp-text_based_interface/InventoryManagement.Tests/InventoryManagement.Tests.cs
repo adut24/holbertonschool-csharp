0d8ca0da51fd55816246bb2095f2dd97523b985c
adut24 <dutertre.alexandre@laposte.net>
2023-07-26 16:08:07 +0200

    Modify the way the path is obtained

--- csharp-text_based_interface/InventoryManagement.Tests/InventoryManagement.Tests.cs
+++ csharp-text_based_interface/InventoryManagement.Tests/InventoryManagement.Tests.cs
@@ -13,23 +13,24 @@ namespace InventoryManagement.Tests
         [OneTimeSetUp]
         public void Setup()
         {
-            // string parentDirectory = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory));
-            // Directory.SetCurrentDirectory(Path.Combine(parentDirectory, "InventoryManager"));
+            string parentDirectory = Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory));
+
+            Directory.SetCurrentDirectory(Path.Combine(parentDirectory, "../../InventoryManager"));
             _inventoryManager = new InventoryManager();
         }
 
-        // [Test]
-        // public void TestShowValidInput()
-        // {
-        //     const string expectedOutput = "{\"name\":\"computer\"," +
-        //     "\"description\":\"personal computer\",\"price\":132.5," +
-        //     "\"tags\":[\"tag1\",\"tag2\"],\"id\":\"b07ab8e4-85cb-44d4-a016-2aae7ba49604\"," +
-        //     "\"date_created\":\"2023-07-16T11:30:44.559012+02:00\",\"date_updated\":\"2023-07-16T11:55:11.211639+02:00\"}";
+        [Test]
+        public void TestShowValidInput()
+        {
+            const string expectedOutput = "{\"name\":\"computer\"," +
+            "\"description\":\"personal computer\",\"price\":132.5," +
+            "\"tags\":[\"tag1\",\"tag2\"],\"id\":\"b07ab8e4-85cb-44d4-a016-2aae7ba49604\"," +
+            "\"date_created\":\"2023-07-16T11:30:44.559012+02:00\",\"date_updated\":\"2023-07-16T11:55:11.211639+02:00\"}";
 
-        //     string output = RunConsoleWithInput("Delete Item b07ab8e4-85cb-44d4-a016-2aae7ba49604");
+            string output = RunConsoleWithInput("Show Item b07ab8e4-85cb-44d4-a016-2aae7ba49604");
 
-        //     Assert.AreEqual(expectedOutput, output.Trim());
-        // }
+            Assert.AreEqual(expectedOutput, output.Trim());
+        }
 
         // [Test]
         // public void TestShowValidInput2()
